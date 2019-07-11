db39a5a75df3c5984fd06ad623e43a49d7899768
Enrico Rossetti <hairic95@gmail.com>
2019-07-11 17:39:22 +0200

    started high level network

--- src/Scenes/Lobby/LobbyScene.gd
+++ src/Scenes/Lobby/LobbyScene.gd
@@ -1,4 +1,7 @@
 extends Control
 
-func _ready():
-	pass
+func _on_HostMatch_pressed():
+	NETWORKING.become_host(int($ConnectionPanel/Port.text))
+
+func _on_JoinMatch_pressed():
+	NETWORKING.join_host($ConnectionPanel/IPAddress.text, int($ConnectionPanel/Port.text))
